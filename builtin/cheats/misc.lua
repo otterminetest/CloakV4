8e3e658a2263b75da7b675e2d1d8ba45dcfa508f
plus-22 <minus22personalacc@gmail.com>
2026-01-24 13:13:24 -0300

    merge flagaura into misc and remove a lot of globalsteps

--- builtin/cheats/misc.lua
+++ builtin/cheats/misc.lua
@@ -1,6 +1,68 @@
+-- Do this first so you can update infontext later
+minetest.register_cheat_with_infotext("FlagAura", "Misc", "flagaura", "")
+minetest.register_cheat_setting("Radius", "Misc", "flagaura", "flagaura.range", {type="slider_float", min=1, max=6, steps=6})
+core.register_cheat_description("FlagAura", "Misc", "flagaura", "Automatically picks up flags in CTF.")
+
 local heal_cooldown = 0
--- AutoHeal
+
+local function destroy_apple(pos)
+    minetest.dig_node(pos)
+end
+
+local spam_active = false
+
+local function spam()
+    if minetest.settings:get_bool("spammer") then
+        minetest.send_chat_message(minetest.settings:get("spammer.message"))
+        minetest.after(tonumber(minetest.settings:get("spammer.cooldown") or 5), spam)
+    else
+        spam_active = false -- stop spam loop when disabled
+    end
+end
+
+local message_sent_combat_target_hud = false
+local message_sent_coords
+
+local timer = 0
+
+local function getOtherColors(excludedColor)
+    local teamColors = {"red", "blue", "green", "orange", "purple"}
+    local otherColors = {}
+    for _, color in ipairs(teamColors) do
+        if color ~= excludedColor then
+            table.insert(otherColors, color)
+        end
+    end
+
+    return otherColors
+end
+
+local function findFlags()
+	local player = minetest.localplayer
+    if not player then return end
+    local tcolor = player:get_teamcolor()
+    if not tcolor then return end
+    local availableColors = getOtherColors(tcolor)
+    local nodeNames = {}
+
+    -- Add the flag nodes for each available color
+    for _, color in ipairs(availableColors) do
+        table.insert(nodeNames, "ctf_modebase:flag_top_" .. color)
+    end
+    local foundNodes = {}
+    for _, nodeName in ipairs(nodeNames) do
+        local nodes = minetest.find_nodes_near(player:get_pos(), tonumber(minetest.settings:get("flagaura.range")), {nodeName})
+        for _, pos in ipairs(nodes) do
+            table.insert(foundNodes, pos) 
+        end
+    end
+
+    return foundNodes
+end
+
 core.register_globalstep(function(dtime)
+
+    -- AutoHeal
 	if core.localplayer then
 		if heal_cooldown > 0 then
 			heal_cooldown = heal_cooldown - dtime
@@ -33,26 +95,11 @@ core.register_globalstep(function(dtime)
 			end
 		end
 	end
-end)
-
-core.register_cheat_with_infotext("AutoHeal", "Misc", "auto_heal", "CTF")
-core.register_cheat_description("AutoHeal", "Misc", "auto_heal", "Automatically eat food if health is below a set value.")
-
-core.register_cheat_setting("Delay", "Misc", "auto_heal", "auto_heal.delay", {type="slider_float", min=0.0, max=1.5, steps=16})
-core.register_cheat_setting("Cooldown", "Misc", "auto_heal", "auto_heal.cooldown", {type="slider_float", min=0.0, max=1.5, steps=16})
-core.register_cheat_setting("HP", "Misc", "auto_heal", "auto_heal.hp", {type="slider_int", min=1, max=30, steps=30})
-
--- AppleAura
-local function destroy_apple(pos)
-    minetest.dig_node(pos)
-end
-
-minetest.register_globalstep(function()
     local player = minetest.localplayer
     if not player then
         return
     end
-
+    --AppleAura
     if minetest.settings:get_bool("appleaura") then
         local player_pos = player:get_pos()
         local apple_nodes = minetest.find_nodes_near(player_pos, tonumber(minetest.settings:get("appleaura.range")), {"default:apple"}) -- Find apples within specified range
@@ -63,43 +110,12 @@ minetest.register_globalstep(function()
             end
         end
     end
-end)
-  
-
-minetest.register_cheat_with_infotext("AppleAura", "Misc", "appleaura", "CTF")
-minetest.register_cheat_setting("Radius", "Misc", "appleaura", "appleaura.range", {type="slider_float", min=1, max=6, steps=6})
-core.register_cheat_description("AppleAura", "Misc", "appleaura", "Automatically digs all apples within a specific radius.")
-
-local spam_active = false
-
-local function spam()
-    if minetest.settings:get_bool("spammer") then
-        minetest.send_chat_message(minetest.settings:get("spammer.message"))
-        minetest.after(tonumber(minetest.settings:get("spammer.cooldown") or 5), spam)
-    else
-        spam_active = false -- stop spam loop when disabled
-    end
-end
-
-minetest.register_globalstep(function()
-    if minetest.settings:get_bool("spammer") and not spam_active then
+    --Spammer
+     if minetest.settings:get_bool("spammer") and not spam_active then
         spam_active = true
         spam()
     end
-end)
-
-
-
-
-minetest.register_cheat("Spammer", "Misc", "spammer")
-core.register_cheat_setting("Cooldown", "Misc", "spammer", "spammer.cooldown", {type="slider_int", min=1, max=50, steps=50})
-core.register_cheat_setting("Text", "Misc", "spammer", "spammer.message", {type="text", size=10})
-
---could look better but it works
-local message_sent_combat_target_hud = false
-local message_sent_coords
-
-minetest.register_globalstep(function()
+    --HUD elements advice thing
     if minetest.settings:get_bool("enable_combat_target_hud") and minetest.settings:get_bool("hud_elements_advice") then
         if not message_sent_combat_target_hud then
             local message = minetest.colorize("#3250af", "[Advice]: To modify this HUD element's (and some others) position and size, open the Click GUI (F8 by default), press 'Edit HUD' button and then you can modify them. You can hide this message with the command .hide_hud_elements_advice")
@@ -109,43 +125,7 @@ minetest.register_globalstep(function()
     else
 		message_sent_combat_target_hud = false
 	end
-end)
-
-minetest.register_globalstep(function()
-    if minetest.settings:get_bool("coords") and minetest.settings:get_bool("hud_elements_advice") then
-        if not message_sent_coords then
-            local message = minetest.colorize("#3250af", "[Advice]: To modify this HUD element's (and some others) position and size, open the Click GUI (F8 by default), press 'Edit HUD' button and then you can modify them. You can hide this message with the command .hide_hud_elements_advice")
-            ws.dcm(message)
-            message_sent_coords = true
-        end
-    else
-		message_sent_coords = false
-	end
-end)
-
-minetest.register_chatcommand("hide_hud_elements_advice", {
-    func = function()
-        core.settings:set_bool("hud_elements_advice", false)
-    end,
-})
-
--- I don't know why you'd wanna use this
-minetest.register_chatcommand("show_hud_elements_advice", {
-    func = function()
-        core.settings:set_bool("hud_elements_advice", true)
-    end,
-})
-
--- Literally the greatest thing ever, props to whoever added it -plus22
-minetest.register_chatcommand("lenny", {
-    func = function()
-        core.send_chat_message("( ͡° ͜ʖ ͡°)")
-    end,
-})
-
-local timer = 0
-
-core.register_globalstep(function(dtime)
+    --Anti AFK
     timer = timer + dtime
 
     if core.settings:get_bool("anti_afk") then
@@ -166,4 +146,52 @@ core.register_globalstep(function(dtime)
             timer = 0
         end
     end
+    --FlagAura
+    if not minetest.settings:get_bool("flagaura") then return end
+    if core.get_server_game() == "not_initialized" then return end
+    if core.get_server_game() ~= "capturetheflag" then
+        core.update_infotext("FlagAura", "Misc", "flagaura", "Invalid Game")
+        return
+    end
+	local positions = findFlags()
+    if not positions then return end
+	for _, pos in ipairs(positions) do
+    	minetest.dig_node(pos)
+	end
 end)
+--AutoHeal
+core.register_cheat_with_infotext("AutoHeal", "Misc", "auto_heal", "CTF")
+core.register_cheat_description("AutoHeal", "Misc", "auto_heal", "Automatically eat food if health is below a set value.")
+
+core.register_cheat_setting("Delay", "Misc", "auto_heal", "auto_heal.delay", {type="slider_float", min=0.0, max=1.5, steps=16})
+core.register_cheat_setting("Cooldown", "Misc", "auto_heal", "auto_heal.cooldown", {type="slider_float", min=0.0, max=1.5, steps=16})
+core.register_cheat_setting("HP", "Misc", "auto_heal", "auto_heal.hp", {type="slider_int", min=1, max=30, steps=30})
+--AppleAura
+minetest.register_cheat_with_infotext("AppleAura", "Misc", "appleaura", "CTF")
+minetest.register_cheat_setting("Radius", "Misc", "appleaura", "appleaura.range", {type="slider_float", min=1, max=6, steps=6})
+core.register_cheat_description("AppleAura", "Misc", "appleaura", "Automatically digs all apples within a specific radius.")
+--Spammer
+minetest.register_cheat("Spammer", "Misc", "spammer")
+core.register_cheat_setting("Cooldown", "Misc", "spammer", "spammer.cooldown", {type="slider_int", min=1, max=50, steps=50})
+core.register_cheat_setting("Text", "Misc", "spammer", "spammer.message", {type="text", size=10})
+
+
+--Commands
+
+minetest.register_chatcommand("hide_hud_elements_advice", {
+    func = function()
+        core.settings:set_bool("hud_elements_advice", false)
+    end,
+})
+
+-- I don't know why you'd wanna use this
+minetest.register_chatcommand("show_hud_elements_advice", {
+    func = function()
+        core.settings:set_bool("hud_elements_advice", true)
+    end,
+})
+minetest.register_chatcommand("lenny", {
+    func = function()
+        core.send_chat_message("( ͡° ͜ʖ ͡°)")
+    end,
+})
